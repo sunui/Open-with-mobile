227b0896f5670941e45c83b9944ed39100f8523a
sunui <776766759@qq.com>
2024-04-17 00:23:14 +0800

    manifest_version 3

--- popup.js
+++ popup.js
@@ -1,21 +1,22 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    chrome.tabs.query({
-        active: true,
-        lastFocusedWindow: true 
-    }, function(array_of_Tabs) {
-        var tab = array_of_Tabs[0];
-        var url = tab.url;
-        var title = tab.title;
-        document.getElementById("title").innerHTML=("扫码打开:<br/>"+title)
-        var qrcode = new QRCode(document.getElementById("qrcode"), {
-            text: url,
-            width: 128,
-            height: 128,
-            colorDark : "#596f5c",
-            colorLight : "#ffffff",
-            correctLevel : QRCode.CorrectLevel.H
-        });
-    });
-
+  chrome.tabs.query(
+    {
+      active: true,
+      lastFocusedWindow: true,
+    },
+    function (array_of_Tabs) {
+      var tab = array_of_Tabs[0];
+      var url = tab.url;
+      var title = tab.title;
+      document.getElementById('title').innerHTML = '扫码打开:<br/>' + title;
+      new QRCode(document.getElementById('qrcode'), {
+        text: url,
+        width: 128,
+        height: 128,
+        colorDark: '#596f5c',
+        colorLight: '#ffffff',
+        correctLevel: QRCode.CorrectLevel.H,
+      });
+    }
+  );
 });
